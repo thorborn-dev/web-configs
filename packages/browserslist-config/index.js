f2aeda8b3f9ae1611c8a9432c3aa1ff728585f38
davidthorand <david.thorand@thorborn.com>
2022-05-15 22:34:11 +0200

    Use prettier

--- packages/browserslist-config/index.js
+++ packages/browserslist-config/index.js
@@ -1,10 +1,10 @@
 module.exports = [
-  "last 3 chrome versions",
-  "last 3 firefox versions",
-  "last 3 safari major versions",
-  "last 3 edge versions",
-  "last 3 opera versions",
-  "last 3 chromeandroid versions",
-  "last 3 firefoxandroid versions",
-  "last 3 ios major versions",
+  'last 3 chrome versions',
+  'last 3 firefox versions',
+  'last 3 safari major versions',
+  'last 3 edge versions',
+  'last 3 opera versions',
+  'last 3 chromeandroid versions',
+  'last 3 firefoxandroid versions',
+  'last 3 ios major versions',
 ];
